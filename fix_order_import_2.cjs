a07323fc4c8bf5bd36bcb4bcee1d8a00a9704f63
sukunarustudio <sukunarustudio@gmail.com>
2026-09-03 16:34:52 +0700

    chore: clean up obsolete scripts, temporary database dumps, and organize test files

--- fix_order_import_2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/views/OrdersView.tsx', 'utf8');
-
-// Replace the first occurrence of CheckCircleIcon,
-c = c.replace('CheckCircleIcon, ', '');
-
-fs.writeFileSync('src/views/OrdersView.tsx', c);
-console.log('Fixed OrdersView.tsx properly');
